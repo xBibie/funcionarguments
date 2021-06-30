2e4b75cffec8de18fff8dbf05ff3125922cadda7
Bibi <84073518+xBibie@users.noreply.github.com>
2021-06-30 21:23:46 +0200

    Add files via upload

---
+++ paintwall.js
@@ -0,0 +1,22 @@
+const paintWall = function (item) {
+    console.log ('paint wall'+ item);
+    console.log ('The wall has been painted'+ item);
+    console.log ('The color' +item 'has been put on the wall');
+};
+
+paintWall ('green');
+paintWall ('blue');
+
+const mainColor = 'Pink';
+
+paintWall (mainColor);
+
+
+const paintWall = function (place, color) {
+    console.log ('paint the wall on the' +place, 'is the the color' +color);
+    console.log ('The wall on the'+place, 'has been painted' +color);
+};
+ const place = 'left';
+ const color = 'pink';
+
+
